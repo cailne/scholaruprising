972b76bef1739c6219d3ab9969142f34c95a3da5
cailne <leonardowijaya10@gmail.com>
2017-10-24 23:16:36 +0700

    2 gucio's code

---
+++ GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    private Player plyr;
+
+    void Start()
+    {
+
+        plyr = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (null == plyr)
+        {
+            Start();
+        }
+        plyr.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        plyr.grounded = false;
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float maxSpeed = 3f;
+    public float speed = 50f;
+    public float jumpPower = 50f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2dp;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2dp = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Grounded", true);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rb2dp.AddForce((Vector2.right * speed) * x);
+
+        if(rb2dp.velocity.x > maxSpeed)
+        {
+            rb2dp.velocity = new Vector2(maxSpeed, rb2dp.velocity.y);
+        }
+        if (rb2dp.velocity.x < -maxSpeed)
+        {
+            rb2dp.velocity = new Vector2(-maxSpeed, rb2dp.velocity.y);
+        }
+    }
+}
