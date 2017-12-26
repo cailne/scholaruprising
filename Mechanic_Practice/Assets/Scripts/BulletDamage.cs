98ca0b6d456350c10653ab608e1587e8a2d0253d
cailne <leonardowijaya10@gmail.com>
2017-12-26 14:26:02 +0700

    Enemy Works ??
    1132 changes because of re-import

---
+++ Mechanic_Practice/Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (!col.isTrigger) {
+			if (col.gameObject.tag != "Enemy") {
+				Destroy (gameObject);
+			}
+		}
+		if (col.gameObject.tag == "Player")
+		{
+			col.SendMessage("stagger");
+		}
+	}
+}
---
+++ Mechanic_Practice/Assets/Scripts/EnemyTurretAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurretAttack : MonoBehaviour {
+
+	public EnemyTurret enemy;
+
+	//public bool isRight = false;
+
+	void Awake(){
+		enemy = gameObject.GetComponentInParent<EnemyTurret> ();
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+
+		if (col.gameObject.tag == "Player") {
+
+			enemy.Attack ();
+
+		}
+
+	}
+}
